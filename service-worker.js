985a4582316a62b2740ddc7ec5e4d90f96d94fb4
voidful <voidful@users.noreply.github.com>
2020-06-30 08:43:13 +0000

    deploy: ea224c8950417cbc4c838d060adad2b978566591

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taiwan-company-network/precache-manifest.a83016a92bf8feab2394416594cf76cc.js"
+  "/taiwan-company-network/precache-manifest.3294897f433647e25d505d4a92dd53a4.js"
 );
 
 self.addEventListener('message', (event) => {
